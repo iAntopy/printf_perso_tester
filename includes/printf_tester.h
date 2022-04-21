2b398a1ab04efb073583b5523e00fc30c72b920a
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2022-04-20 20:50:57 -0400

    Slight restructuring, added includes/ and outputs directories.

---
+++ includes/printf_tester.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_tester.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iamongeo <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/20 16:37:34 by iamongeo          #+#    #+#             */
+/*   Updated: 2022/04/20 17:22:47 by iamongeo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_TESTER_H
+# define PRINTF_TESTER_H
+
+# include <stdio.h>
+# include <limits.h>
+# include "../../ft_printf.h"
+
+void	d_test_ft(void);
+void	d_test_pt(void);
+void	i_test_ft(void);
+void	i_test_pt(void);
+void	c_test_ft(void);
+void	c_test_pt(void);
+void	s_test_ft(void);
+void	s_test_pt(void);
+void	u_test_ft(void);
+void	u_test_pt(void);
+void	x_test_ft(void);
+void	x_test_pt(void);
+void	X_test_ft(void);
+void	X_test_pt(void);
+void	p_test_ft(void);
+void	p_test_pt(void);
+void	pct_test_ft(void);
+void	pct_test_pt(void);
+
+#endif
